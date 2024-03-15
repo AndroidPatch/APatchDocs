5688034df980ccbbce78fb28bf387186bd82f5f6
NightSong_UwU <2389839752@qq.com>
2024-03-16 01:15:12 +0800

    更新 config.mjs

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  base:'/APatchDocs/',
   title: "APatch",
   description: "APatch文档",
   themeConfig: {
